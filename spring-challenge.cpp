6f599e2e5c04c533729a6fd197b416b3b522b29c
Akshat <akshatbhargavadeepa@gmail.com>
2020-05-09 04:27:43 +0530

    revamped a bit of cpp code, also editor formatted

--- spring-challenge.cpp
+++ spring-challenge.cpp
@@ -5,124 +5,198 @@
 
 using namespace std;
 
-struct Position {
+struct Position
+{
     int x;
     int y;
 };
 
-struct Pac {
-    int pacId; // pac number (unique within a team)
-    Position pos; // position of pac in grid
-    string typeId; // unused in wood leagues
-    int speedTurnsLeft; // unused in wood leagues
+struct Pac
+{
+    int pacId;           // pac number (unique within a team)
+    Position pos;        // position of pac in grid
+    string typeId;       // unused in wood leagues
+    int speedTurnsLeft;  // unused in wood leagues
     int abilityCooldown; // unused in wood leagues
 };
 
-struct Pellet {
+struct Pellet
+{
     Position pos; // pellet position in grid
-    int value; // amount of points this pellet is worth
+    int value;    // amount of points this pellet is worth
 };
 
-class GameState {
+class GameState
+{
 public:
-    int WIDTH, HEIGHT;
+    int width, height;
     int myScore, opponentScore;
-    vector<string> grid;
+    vector<vector<char>> grid;
+    vector<vector<char>> universalGrid;
     vector<Pac> pacs, enemies;
     vector<Pellet> pellets;
 
-    GameState() {
+    GameState()
+    {
         initGrid();
     }
 
-    void initGrid() {
-        cin >> WIDTH >> HEIGHT; cin.ignore();
-        for (int i = 0; i < HEIGHT; i++) {
+    void initGrid()
+    {
+        cin >> width >> height;
+        cin.ignore();
+
+        vector<vector<char>> tempGrid(height, vector<char>(width, ' '));
+
+        for (int i = 0; i < height; i++)
+        {
             string row;
-            getline(cin, row); // one line of the grid: space " " is floor, pound "#" is wall
-            grid.push_back(row);
+            getline(cin, row);
+            for (int j = 0; j < width; j++)
+            {
+                tempGrid[i][j] = row[j];
+            }
         }
+
+        this->grid = tempGrid;
     }
 
-    void initGameState() {
+    initUniversalGrid()
+    {
+        vector<vector<char>> tUniversalGrid(height, vector<char>(width, ' '));
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (grid[i][j] == ' ')
+                    universalGrid[i][j] = 'o';
+                else if (grid[i][j] == '#')
+                    universalGrid[i][j] = '#';
+            }
+        }
+
+        for (Pac pac : this->pacs)
+        {
+            for (int x = pac.pos.x; x >= 0; x--)
+                tUniversalGrid[pac.pos.y][x] = grid[pac.pos.y][x];
+            for (int y = pac.pos.y; y >= 0; y--)
+                tUniversalGrid[y][pac.pos.x] = grid[y][pac.pos.x];
+
+            for (int x = pac.pos.x; x < width; x++)
+                tUniversalGrid[y][pac.pos.x] = grid[y][pac.pos.x];
+            for (int y = pac.pos.y; y < height; y++)
+                tUniversalGrid[y][pac.pos.x] = grid[y][pac.pos.x];
+        }
+
+        for (Pellet pellet : pellets)
+        {
+            universalGrid[pellet.pos.y][pellet.pos.x] = pellet.value == 10 ? 'O' : 'o';
+        }
+
+        this->universalGrid = tUniversalGrid;
+    }
+
+    void initGameState()
+    {
         pacs.clear();
         enemies.clear();
         pellets.clear();
         removeStateFromGrid();
 
-        cin >> myScore >> opponentScore; cin.ignore();
+        cin >> myScore >> opponentScore;
+        cin.ignore();
 
         int visiblePacCount;
-        cin >> visiblePacCount; cin.ignore();
+        cin >> visiblePacCount;
+        cin.ignore();
 
-        for (int i = 0; i < visiblePacCount; i++) {
-            int pacId; // pac number (unique within a team)
-            bool mine; // true if this pac is yours
-            int x; // position in the grid
-            int y; // position in the grid
-            string typeId; // unused in wood leagues
-            int speedTurnsLeft; // unused in wood leagues
-            int abilityCooldown; // unused in wood leagues
-            cin >> pacId >> mine >> x >> y >> typeId >> speedTurnsLeft >> abilityCooldown; cin.ignore();
+        for (int i = 0; i < visiblePacCount; i++)
+        {
+            int pacId, x, y, speedTurnsLeft, abilityCooldown;
+            bool mine;
+            string typeId;
+            cin >> pacId >> mine >> x >> y >> typeId >> speedTurnsLeft >> abilityCooldown;
+            cin.ignore();
 
-            if (mine) grid[y][x] = '';
-            else grid[y][x] = 'E';
-
-            if (mine) {
-                pacs.push_back(Pac{pacId,{x,y},typeId,speedTurnsLeft,abilityCooldown});
-            } else {
-                enemies.push_back(Pac{pacId,{x,y},typeId,speedTurnsLeft,abilityCooldown});
+            if (mine)
+            {
+                pacs.push_back(Pac{pacId, {x, y}, typeId, speedTurnsLeft, abilityCooldown});
             }
+            else
+            {
+                enemies.push_back(Pac{pacId, {x, y}, typeId, speedTurnsLeft, abilityCooldown});
+            }
+
+            if (mine)
+                grid[y][x] = 'P';
+            else
+                grid[y][x] = 'E';
         }
 
-        int visiblePelletCount; // all pellets in sight
-        cin >> visiblePelletCount; cin.ignore();
-        for (int i = 0; i < visiblePelletCount; i++) {
-            int x;
-            int y;
-            int value; // amount of points this pellet is worth
-            cin >> x >> y >> value; cin.ignore();
-            pellets.push_back(Pellet{{x,y}, value});
-            if (grid[y][x] != 'E' && grid[y][x] != 'P') grid[y][x] = value == 10 ? 'O' : 'o';
+        int visiblePelletCount;
+        cin >> visiblePelletCount;
+        cin.ignore();
+        for (int i = 0; i < visiblePelletCount; i++)
+        {
+            int x, y, value;
+            cin >> x >> y >> value;
+            cin.ignore();
+
+            pellets.push_back(Pellet{{x, y}, value});
+
+            grid[y][x] = value == 10 ? 'O' : 'o';
         }
     }
 
-    void removeStateFromGrid() {
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[i].size(); j++) {
-                if (grid[i][j] != '#') grid[i][j] = ' ';
+    void removeStateFromGrid()
+    {
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[i].size(); j++)
+            {
+                if (grid[i][j] != '#')
+                    grid[i][j] = ' ';
             }
         }
     }
 };
 
-class GameAI {
+class GameAI
+{
 private:
     GameState *gameState;
+
 public:
-    GameAI(GameState *_gameState) {
+    GameAI(GameState *_gameState)
+    {
         gameState = _gameState;
     }
 
-    void debugGrid() {
-        const vector<string> grid = gameState->grid;
-        for (auto row: grid) {
-            for (char ch: row) {
-                cerr << ch << " ";
+    void debugGrid()
+    {
+        const vector<vector<char>> grid = gameState->grid;
+        for (auto row : grid)
+        {
+            for (char ch : row)
+            {
+                cerr << ch;
             }
             cerr << endl;
         }
     }
 
-    void nextMove() {
+    void nextMove()
+    {
         vector<Pac> &pacs = gameState->pacs;
         vector<Pellet> &pellets = gameState->pellets;
         sort(pellets.begin(), pellets.end(), [](Pellet p1, Pellet p2) {
             return p1.value > p2.value;
         });
-        for (int i = 0; i < pacs.size(); i++) {
+        for (int i = 0; i < pacs.size(); i++)
+        {
             cout << "MOVE " << pacs[i].pacId << " " << pellets[i].pos.x << " " << pellets[i].pos.y;
-            if (i != pacs.size() - 1) cout << "|";
+            if (i != pacs.size() - 1)
+                cout << "|";
         }
         cout << endl;
     }
@@ -134,7 +208,8 @@ int main()
     GameState gameState = GameState();
     GameAI ai{&gameState};
 
-    while (1) {
+    while (1)
+    {
         gameState.initGameState();
         ai.debugGrid();
         ai.nextMove();
